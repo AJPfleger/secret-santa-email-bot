d6ab4e0a8ce747592a71ba544c5a907b4dd03571
AJPfleger <alexander.julian.pfleger@cern.ch>
2023-11-25 12:20:14 +0100

    refactor

--- secretsantabot.py
+++ secretsantabot.py
@@ -15,69 +15,11 @@ Sources:
 import smtplib
 import ssl
 from email.mime.text import MIMEText
-import xlrd
+import xlrd  # == 1.2.0 because they removed support for xlsx later
 import random
 
-exp_cols = 8
 
-# read data from excel
-excel_sheet = xlrd.open_workbook("participants.xlsx").sheet_by_index(0)
-# print(excel_sheet.cell_value(0, 0))
-
-n_participants = excel_sheet.nrows
-
-assert excel_sheet.ncols == exp_cols  # check if xlsx has proper dimension
-assert n_participants > 2  # check if enough participants
-
-# generate pairings
-partA = list(range(1, n_participants))
-partB = partA.copy()
-# badpair = 1
-# while badpair != 0:
-#    badpair = 0
-#    random.shuffle(partB)
-#    for k in range(n_participants-1):
-#        badpair += (partA[k] == partB[k])
-while any(a == b for (a, b) in zip(partA, partB)):
-    random.shuffle(partB)
-
-# send mail
-port = 465  # For SSL
-smtp_server = "smtp.randomserver.xyz"
-sender_email = "email@email.xyz"  # Enter your address
-password = input("Type your password and press enter: ")
-
-
-"""
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
-"""
-
-
-rawmessage = """
-Liebe(r) {nameA}!
-
-Schön, dass Du dieses Jahr beim AFÖP-Wichteln dabei bist. Bitte bereite ein Geschenk für {nameB} vor aber sags nicht \
-weiter, wen Du beschenkst, damit alle mehr Freude am Wichteln haben. Um ein passendes Geschenk zu finden und es auch \
-rechtzeitig zu übergeben hat Dir Dein Wichtel noch ein paar Informationen hinterlassen.
-
-Dein Wichtel würde das Geschenk am liebsten so erhalten:
-{adresse}
-
-Falls Du etwas Essbares schenken willst beachte bitte noch:
-{food}
-
-Voraussichtlich ist {nameB} noch bis {date} in Wien. Also beeile Dich, damit Dein Geschenk rechtzeitig ankommt!
-
-Im äußersten Notfall kannst {nameB} unter {mailB} erreichen, aber das könnte die Überraschung verderben.
-Abschließen möchte Dir Dein Wichtel noch folgendes sagen:
-{comment}
-
-Ich wünsche Dir noch viel Spaß beim Schenken und Beschenkt werden.
-Dein Wichtelbot
-
+picture = """
                                                ._...._.
                        \\ .*. /                .::o:::::.
                         (\\o/)                .:::'''':o:.
@@ -102,33 +44,92 @@ ________________________________________________________________
 
 """
 
+
+def create_message(nameA, nameB, mailB, date, adresse, food, comment):
+    msg_str = f"Liebe(r) {nameA}!\n\n"
+    msg_str += (
+        f"Schön, dass Du dieses Jahr beim AFÖP-Wichteln dabei bist. Bitte bereite ein Geschenk für {nameB} vor aber "
+        f"sags nicht weiter, wen Du beschenkst, damit alle mehr Freude am Wichteln haben. Um ein passendes Geschenk zu "
+        f"finden und es auch rechtzeitig zu übergeben hat Dir Dein Wichtel noch ein paar Informationen hinterlassen."
+        f"\n\n"
+    )
+
+    if len(adresse) != 0:
+        msg_str += (
+            f"Dein Wichtel würde das Geschenk am liebsten so erhalten:\n{adresse}\n\n"
+        )
+
+    if len(food) != 0:
+        msg_str += f"Falls Du etwas Essbares schenken willst beachte bitte noch (Allergien etc.):\n{food}\n\n"
+
+    if len(date) != 0:
+        msg_str += (
+            f"Voraussichtlich ist {nameB} noch bis {date} in Wien. Also beeile Dich, damit Dein Geschenk rechtzeitig "
+            f"ankommt!\n\n"
+        )
+
+    msg_str += f"Im äußersten Notfall kannst {nameB} unter {mailB} erreichen, aber das könnte die Überraschung verderben.\n\n"
+
+    if len(comment) != 0:
+        msg_str += (
+            f"Abschließen möchte Dir Dein Wichtel noch folgendes sagen:\n{comment}\n\n"
+        )
+
+    msg_str += "Ich wünsche Dir noch viel Spaß beim Schenken und Beschenkt werden.\n\tDein Wichtelbot\n\n"
+    msg_str += picture
+
+    return msg_str
+
+
+exp_cols = 8
+
+# read data from excel
+excel_sheet = xlrd.open_workbook("participants.xlsx").sheet_by_index(0)
+
+n_participants = excel_sheet.nrows
+
+assert excel_sheet.ncols == exp_cols  # check if xlsx has proper dimension
+assert n_participants > 2  # check if enough participants
+
+# generate pairings
+partA = list(range(1, n_participants))
+partB = partA.copy()
+# badpair = 1
+# while badpair != 0:
+#    badpair = 0
+#    random.shuffle(partB)
+#    for k in range(n_participants-1):
+#        badpair += (partA[k] == partB[k])
+while any(a == b for (a, b) in zip(partA, partB)):
+    random.shuffle(partB)
+
+# send mail
+port = 465  # For SSL
+smtp_server = "smtp.domainname.xyz"  # Enter your smtp_server
+sender_email = "sender@mail.xyz"  # Enter your sender_email
+password = input("Type your password and press enter: ")
+
+
 for pair in range(n_participants - 1):
     receiver_email = excel_sheet.cell_value(partA[pair], 3)
 
-    message = rawmessage.format(
-        nameA=excel_sheet.cell_value(partA[pair], 2),
-        nameB=excel_sheet.cell_value(partB[pair], 2),
-        mailB=excel_sheet.cell_value(partB[pair], 3),
-        date=excel_sheet.cell_value(partB[pair], 4),
-        adresse=excel_sheet.cell_value(partB[pair], 5),
-        food=excel_sheet.cell_value(partB[pair], 6),
-        comment=excel_sheet.cell_value(partB[pair], 7),
+    message = create_message(
+        excel_sheet.cell_value(partA[pair], 2),
+        excel_sheet.cell_value(partB[pair], 2),
+        excel_sheet.cell_value(partB[pair], 3),
+        excel_sheet.cell_value(partB[pair], 4),
+        excel_sheet.cell_value(partB[pair], 5),
+        excel_sheet.cell_value(partB[pair], 6),
+        excel_sheet.cell_value(partB[pair], 7),
     )
 
     print(receiver_email)
     print(message)
 
-    """
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, message)
-    """
-
     msg = MIMEText(message.encode("utf-8"), _charset="utf-8")
     msg["Subject"] = "Wichteln 2023"
     msg["From"] = sender_email
-    msg["To"] = "mail@mail.com"
+    msg["To"] = receiver_email
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
         server.login(sender_email, password)
